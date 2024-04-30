feb6bd85b5cbf8953edf335fcd73da0483c9ffa5
Claudia Simone de Souza <claudia.simmone@gmail.com>
2024-04-30 15:07:29 +0100

    20240430 work.

--- E12_ParteInteira_v01/Program.cs
+++ E12_ParteInteira_v01/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using D00_Utility;
 
 namespace E12_ParteInteira_v01
 {
@@ -10,7 +11,24 @@ namespace E12_ParteInteira_v01
     {
         static void Main(string[] args)
         {
+            Utility.WriteTitle("Apresente no ecrã a parte inteira de um número introduzido pelo utilizador", "", "\n\n");
 
+            double dec;
+            int integer;
+            bool status = false;
+
+            do
+            {
+                Utility.WriteMessage("Insira o número decimal: ");
+                status = double.TryParse(Console.ReadLine(), out dec);
+
+            } while (status != true);
+
+            integer = Convert.ToInt16(dec);
+
+            Utility.WriteMessage($"O número decimal {dec} tem o numero {integer} como inteiro ", "\n", "\n");
+
+            Utility.TerminateConsole();
         }
     }
 }
