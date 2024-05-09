c4655cb796f281d0527f234bbf054eb28092d792
Claudia Simone de Souza <claudia.simmone@gmail.com>
2024-05-09 20:00:47 +0100

    20240509 work.

---
+++ D15_Collections_ArrayList/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D00_Utility;
+
+namespace D15_Collections_ArrayList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Utility.SetUnicodeConsole();
+
+            #region ArrayList: multi tipos + inteiros
+
+            // 1. instanciar array list
+            // 2. invoque a classe array list
+
+            ArrayList listMulti = new ArrayList();
+            ArrayList listIntegers = new ArrayList();
+
+            // 3.  
+
+            #endregion
+
+            #region Persons: objects of class Person
+
+            #endregion
+
+            Utility.TerminateConsole();
+
+        }
+    }
+}
