6e311f889ddc6039479dbd5c2f630a36533dc4f4
Claudia Simone de Souza <claudia.simmone@gmail.com>
2024-05-11 14:51:55 +0100

    20240511

---
+++ E17_ValorDeVenda_v01/PriceUtility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D00_Utility;
+
+namespace E17_ValorDeVenda_v01
+{
+    internal class PriceUtility
+    {
+        internal static double CheckDouble(string input)
+        {
+            return;
+        }
+    }
+}
---
+++ E17_ValorDeVenda_v01/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D00_Utility;
+
+namespace E17_ValorDeVenda_v01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Escrever um programa que leia o preço base de um determinado produto e calcule o seu valor de venda ao público(ou seja, valor acrescido da taxa de IVA a 23 %).
+
+            Utility.SetUnicodeConsole();
+
+            
+
+            
+
+
+
+
+            Utility.TerminateConsole();
+        }
+    }
+}
