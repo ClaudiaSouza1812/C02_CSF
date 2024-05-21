b2af11311fc179ea3d891f4583d855899deec8ce
Claudia Simone de Souza <claudia.simmone@gmail.com>
2024-05-21 15:16:16 +0100

    240521

--- E25_ChangeValues_v1/Program.cs
+++ E25_ChangeValues_v1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using D00_Utility;
 
 namespace E25_ChangeValues_v1
 {
@@ -14,10 +15,36 @@ namespace E25_ChangeValues_v1
              18. Escrever um programa que troque entre si o valor contido em duas variáveis inteiras a e b, ou seja, o valor contido em a passa a ser o valor de b e vice-versa, sem utilizar uma terceira variável.
              */
 
+            Utility.SetUnicodeConsole();
 
             int a, b;
+            bool status01, status02;
 
+            do
+            {
+                Console.Clear();
+                Utility.WriteTitle("Trocar entre si o valor contido em duas variáveis inteiras (a) e (b)", "", "\n\n");
 
+                Utility.WriteMessage("Insert the value of (a): ", "", "\n");
+                status01 = int.TryParse(Console.ReadLine(), out a);
+
+                Utility.WriteMessage("Insert the value of (b): ", "", "\n");
+                status02 = int.TryParse(Console.ReadLine(), out b);
+
+                if (status01 & status02)
+                {
+                    Utility.WriteMessage($"O valor de (a) agora é: {b}", "\n", "\n");
+                    Utility.WriteMessage($"O valor de (b) agora é: {a}", "", "\n");
+                }
+                else
+                {
+                    Utility.WriteMessage("Insira números inteiros.", "", "\n");
+                }
+
+            } while (!status01 & !status02);
+            
+
+            Utility.TerminateConsole();
         }
     }
 }
